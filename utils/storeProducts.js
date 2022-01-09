cb85434b7f063d174b5bd3de94b7801a0b42b7af
Annie Wang <wangannie@berkeley.edu>
2022-01-09 13:58:42 -0500

    Integrate Appian API (#4)
    * Working Appian integration
    
    * Update to Appian prod, cleanup
    
    * Add temp. support to keep old non-appian products
    
    * Revert node version
    
    * Update product names
    
    * Clear out Google Sheets endpoints
    
    * Clean up error handling, add script
    
    * Update custom product name mappings

--- utils/storeProducts.js
+++ utils/storeProducts.js
@@ -1,4 +1,5 @@
 /* eslint-disable import/prefer-default-export */
+import moment from 'moment';
 import {
   getAllProducts as getAllProdProducts,
   getAllStores as getAllProdStores,
@@ -9,15 +10,11 @@ import {
   getAllStores as getAllDevStores,
   updateManyStores as updateManyDevStores,
 } from '../lib/airtable/request';
-import { getCurrentStoreProducts, updateDateRange } from './googleSheets';
+import getProducts from './appian';
 
 // Update the store-products linked records mapping in Airtable
-export const updateStoreProducts = async (oAuth2Client, base = 'DEV') => {
-  // First make an API call to update the current date range in the Google sheet
-  await updateDateRange(oAuth2Client);
-
-  // Next, we get parsed data of the updated Google sheet
-  const parsedData = await getCurrentStoreProducts(oAuth2Client);
+export const updateStoreProducts = async (base = 'DEV') => {
+  const parsedData = await getProducts();
 
   let currentStores = [];
   let currentProducts = [];
@@ -43,25 +40,25 @@ export const updateStoreProducts = async (oAuth2Client, base = 'DEV') => {
   const updatedStoreNames = [];
   // eslint-disable-next-line no-restricted-syntax
   for (const storeData of parsedData) {
-    const { name, products, deliveryDate } = storeData;
-    const store = currentStores.find((record) => record.storeName === name);
+    const { storeName, products, lastDeliveryDate } = storeData;
+    const store = currentStores.find(
+      (record) => record.storeName === storeName
+    );
     if (!store) {
-      console.log('Store not found in Airtable '.concat(name));
-      missingStores.push(name);
+      console.log('Store not found in Airtable '.concat(storeName));
+      missingStores.push(storeName);
       // eslint-disable-next-line no-continue
       continue;
     } else {
       const productIds = currentProducts
         .filter((record) => products.includes(record.fullName))
         .map((record) => record.id);
-
       updatedStores.push({
         id: store.id,
-        fields: { productIds, latestDelivery: deliveryDate },
+        fields: { productIds, latestDelivery: lastDeliveryDate },
       });
-
-      // The CSV only contains stores that were delivered to recently
-      updatedStoreNames.push(name);
+      // The response only contains stores that were delivered to recently
+      updatedStoreNames.push(storeName);
 
       // Check if products or missing or incorrectly formatted
       const storeMissingProducts = products.filter(
@@ -74,10 +71,14 @@ export const updateStoreProducts = async (oAuth2Client, base = 'DEV') => {
       });
     }
   }
-
   // Track names for logging purposes
   const noDeliveryStoreNames = [];
-  // Find which stores in the [DEV] base have not had deliveries
+
+  // Temporarily track start/end range to keep old products for Appian transition
+  const startDate = moment().subtract(9, 'days');
+  const endDate = moment();
+
+  // Find which stores in the base have not had deliveries
   const noDeliveryStores = currentStores
     .filter(
       (currentStore) =>
@@ -85,6 +86,22 @@ export const updateStoreProducts = async (oAuth2Client, base = 'DEV') => {
     )
     .map((noDeliveryStore) => {
       noDeliveryStoreNames.push(noDeliveryStore.storeName);
+      // Temporarily (only until transition is complete) keep products that have delivery dates within last 9 days for cutover
+      if (
+        moment(noDeliveryStore.latestDelivery).isBetween(startDate, endDate)
+      ) {
+        console.log(
+          'Keeping products for ',
+          noDeliveryStore.storeName,
+          ' since ',
+          noDeliveryStore.latestDelivery,
+          ' is within last 9 days'
+        );
+        return {
+          id: noDeliveryStore.id,
+          fields: {},
+        };
+      }
       return {
         id: noDeliveryStore.id,
         fields: { productIds: [] },
@@ -92,7 +109,7 @@ export const updateStoreProducts = async (oAuth2Client, base = 'DEV') => {
     });
 
   // Useful logging
-  console.log('\n\n');
+  console.log('\n');
   console.log('\nStores with deliveries this cycle:', updatedStoreNames);
   console.log('\nStores with no deliveries this cycle:', noDeliveryStoreNames);
 
